1dbd953c7ffd2da1021c07f499ae3b7cb31410ef
Nathália Brum <natybrum.97@gmail.com>
2023-05-05 18:25:58 -0300

    Versão com User Pronto

--- src/components/User.js
+++ src/components/User.js
@@ -1,12 +1,34 @@
+import { useState } from "react";
+
 export default function User() {
+
+    let [nome, setNome] = useState('catacomics');
+    let [imagem, setImagem] = useState('assets/img/catanacomics.svg');
+
+    function alterarNome() {
+        let novoNome = prompt('Qual é o seu nome de usuário?')
+        if (!novoNome) {
+            return;
+        }
+        setNome(novoNome);
+    }
+
+    function trocarImagem(){
+        let novaImagem = prompt('Digite o link de uma nova imagem');
+        if (!novaImagem) {
+            return;
+        }
+        setImagem(novaImagem);
+    }
+
     return (
 
         <div class="usuario">
-            <img data-test="profile-image" src="assets/img/catanacomics.svg" alt="imagem de perfil" />
+            <img data-test="profile-image" onClick={trocarImagem} src={imagem} alt="imagem de perfil" />
             <div class="texto">
                 <span>
-                    <strong data-test="name">catanacomics</strong>
-                    <ion-icon data-test="edit-name" name="pencil"></ion-icon>
+                    <strong data-test="name">{nome}</strong>
+                    <ion-icon data-test="edit-name" onClick={alterarNome} name="pencil"></ion-icon>
                 </span>
             </div>
         </div>
